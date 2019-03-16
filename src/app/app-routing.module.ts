19f320bd1c49a4533aa55b26663c5198d9ca7f1d
yachingtang2 <yachingtang@rumbleon.com>
2019-03-15 23:42:08 -0500

    Redo app-routing module.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { TexasLottoComponent } from './texas-lotto/texas-lotto.component';
-import { WelcomeComponent } from './welcome/welcome.component';
+import { RouterModule, Routes } from "@angular/router";
+import { WelcomeComponent } from "./welcome/welcome.component";
+import { NgModule } from "@angular/core";
+import { TexasLottoComponent } from "./texas-lotto/texas-lotto.component";
 
 export const routes: Routes = [
   {
@@ -20,7 +20,35 @@ export const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routes) ],
-  exports: [ RouterModule ]
+  imports: [
+    RouterModule.forRoot(routes)
+  ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+
+// import { NgModule } from '@angular/core';
+// import { Routes, RouterModule } from '@angular/router';
+// import { TexasLottoComponent } from './texas-lotto/texas-lotto.component';
+// import { WelcomeComponent } from './welcome/welcome.component';
+
+// export const routes: Routes = [
+//   {
+//     path: 'welcome',
+//     component: WelcomeComponent
+//   },
+//   {
+//     path: 'texas-lotto',
+//     component: TexasLottoComponent
+//   },
+//   {
+//     path: '',
+//     redirectTo: 'welcome',
+//     pathMatch: 'full'
+//   }
+// ];
+
+// @NgModule({
+//   imports: [ RouterModule.forRoot(routes) ],
+//   exports: [ RouterModule ]
+// })
+// export class AppRoutingModule { }
--- src/app/app-routing.spec.ts
+++ src/app/app-routing.spec.ts
@@ -1,13 +1,13 @@
-import { AppComponent } from './app.component';
-import { routes } from './app-routing.module';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { AppComponent } from "./app.component";
 import { Router } from "@angular/router";
-import { ComponentFixture, fakeAsync, tick, TestBed } from "@angular/core/testing";
-import { Location, CommonModule } from '@angular/common';
-import { RouterTestingModule } from '@angular/router/testing';
-import { WelcomeComponent } from './welcome/welcome.component';
-import { TexasLottoComponent } from './texas-lotto/texas-lotto.component';
+import { Location } from "@angular/common";
+import { RouterTestingModule } from "@angular/router/testing";
+import { routes } from "./app-routing.module";
+import { WelcomeComponent } from "./welcome/welcome.component";
+import { TexasLottoComponent } from "./texas-lotto/texas-lotto.component";
 
-describe('Router Test', () => {
+describe('Router test', () => {
   let fixture: ComponentFixture<AppComponent>;
   let router: Router;
   let location: Location;
@@ -20,35 +20,84 @@ describe('Router Test', () => {
         WelcomeComponent
       ],
       imports: [
-        RouterTestingModule.withRoutes(routes),
-        CommonModule
+        RouterTestingModule.withRoutes(routes)
       ]
     });
     router = TestBed.get(Router);
     location = TestBed.get(Location);
     fixture = TestBed.createComponent(AppComponent);
-    fixture.ngZone.run(() => {
-      router.initialNavigation();
-    });
   });
 
-  it('canary test', () => {
+  it('should create', () => {
     expect(true).toBeTruthy();
   });
 
-  it('should navigate to Home page', fakeAsync(() => {
-    fixture.ngZone.run(() => {
-      router.navigate(['welcome']).then(() => {
-        expect(location.path()).toBe('/welcome');
-      });
-      tick();
-    });
-  }));
+  it('should navigate to Welcome page', () => {
+    fixture.ngZone.run(() => router.navigate(['welcome']))
+      .then(() => expect(location.path()).toBe('/welcome'));
+  });
 
-  it('should navigate to texas-lotto', fakeAsync(() => {
-    fixture.ngZone.run(() => {
-      router.navigate(['texas-lotto']).then(() => expect(location.path()).toBe('/texas-lotto'));
-      tick();
-    });
-  }));
+  it('should navigate to Texas Lotto page', () => {
+    fixture.ngZone.run(() => router.navigate(['texas-lotto'])
+      .then(() => expect(location.path()).toBe('/texas-lotto')));
+  });
+
+  it('should navigate to default page - Welcome page', () => {
+    fixture.ngZone.run(() => router.navigate([''])
+      .then(() => expect(location.path()).toBe('/welcome')));
+  });
 });
+// import { AppComponent } from './app.component';
+// import { routes } from './app-routing.module';
+// import { Router } from "@angular/router";
+// import { ComponentFixture, fakeAsync, tick, TestBed } from "@angular/core/testing";
+// import { Location, CommonModule } from '@angular/common';
+// import { RouterTestingModule } from '@angular/router/testing';
+// import { WelcomeComponent } from './welcome/welcome.component';
+// import { TexasLottoComponent } from './texas-lotto/texas-lotto.component';
+
+// describe('Router Test', () => {
+//   let fixture: ComponentFixture<AppComponent>;
+//   let router: Router;
+//   let location: Location;
+
+//   beforeEach(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [
+//         AppComponent,
+//         TexasLottoComponent,
+//         WelcomeComponent
+//       ],
+//       imports: [
+//         RouterTestingModule.withRoutes(routes),
+//         CommonModule
+//       ]
+//     });
+//     router = TestBed.get(Router);
+//     location = TestBed.get(Location);
+//     fixture = TestBed.createComponent(AppComponent);
+//     fixture.ngZone.run(() => {
+//       router.initialNavigation();
+//     });
+//   });
+
+//   it('canary test', () => {
+//     expect(true).toBeTruthy();
+//   });
+
+//   it('should navigate to Home page', fakeAsync(() => {
+//     fixture.ngZone.run(() => {
+//       router.navigate(['welcome']).then(() => {
+//         expect(location.path()).toBe('/welcome');
+//       });
+//       tick();
+//     });
+//   }));
+
+//   it('should navigate to texas-lotto', fakeAsync(() => {
+//     fixture.ngZone.run(() => {
+//       router.navigate(['texas-lotto']).then(() => expect(location.path()).toBe('/texas-lotto'));
+//       tick();
+//     });
+//   }));
+// });
